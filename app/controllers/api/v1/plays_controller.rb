058ade33b8d77c2d3c7db2de05f6e0954a189bcc
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-21 12:14:19 -0700

    attempting story 4

--- app/controllers/api/v1/plays_controller.rb
+++ app/controllers/api/v1/plays_controller.rb
@@ -3,11 +3,12 @@ class Api::V1::PlaysController < ApplicationController
   def create
     @game = Game.find(params[:game_id])
     @play = @game.plays.create(play_params)
+    OxfordService.new(play_params[:word]).get_word
 
     if @play.save
       render json: @game.plays.last
     else
-      {message: "word is not valid"}
+      {message: "#{params[:word]} is not valid"}
     end
   end
 
