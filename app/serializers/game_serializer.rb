d3ab4e40a4d48a6f55449414ec6916b38000d41f
GabrielAfflitto <gafflitto92@gmail.com>
2018-02-27 21:07:08 -0700

    Added proper output for GET request in Game API

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -2,6 +2,9 @@ class GameSerializer < ActiveModel::Serializer
   attributes :id, :scores
 
   def scores
-    object.plays.select(:user_id, :score).as_json(:except => :id)
+    [{user_id: object.player_1.id,
+      score: object.player_1.plays.sum(:score)},
+     {user_id: object.player_2.id,
+      score: object.player_2.plays.sum(:score)}]
   end
 end
